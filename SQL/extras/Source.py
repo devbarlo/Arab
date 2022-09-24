ac9659862c79db1faab60377d8db057454eaaa65
Wjut <102990724+Wjut@users.noreply.github.com>
2022-09-24 13:56:48 +0200

    Add files via upload

---
+++ SQL/extras/Source.py
@@ -0,0 +1,11 @@
+import re
+import time
+from datetime import datetime
+from userbot import StartTime, ninn2
+from userbot.Config import Config
+from userbot.plugins import mention
+help1 = ("**⎈ ⦙ كيفيه التنصيب :**")
+help2 = ("**⎈ ⦙ قـائمـه الاوامـر :**\n**⎈ ⦙ قنـاه السـورس :** @cr_source \n**⎈ ⦙ شـرح اوامـر السـورس : @q_k_2**\n**⎈ ⦙ شـرح فـارات السـورس : @cr_source** ")
+TG_BOT = Config.TG_BOT_USERNAME
+TM = time.strftime("%I:%M")
+Sour = f"**.𓄌 : version 7.4  𓇡.** \n.𓄌 : me  {mention}  𓇡. \n**.𓄌 : time  {TM}  𓇡.**\n**.𓄌 : My Bot {TG_BOT} 𓇡.**\n**.𓄌 : Source Cristen : @cr_source  𓇡.**"
